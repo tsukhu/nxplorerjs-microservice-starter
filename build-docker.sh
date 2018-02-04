7b3493a874069c1904ff58c1c8d4b10d494537b0
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-02-04 10:38:07 +0530

    Docker test fixes

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t tsukhu/nxplorerjs-microservice .
+docker build -t tksukhu/nxplorerjs-microservice .
--- build.js
+++ build.js
@@ -5,7 +5,7 @@ s.rm('-rf', 'reports');
 s.mkdir('build');
 s.mkdir('reports');
 s.mkdir('-p','logs');
-s.cp(`.${s.env.NODE_ENV}.env`, 'build/.env');
+s.cp(`.${s.env.NODE_ENV}.env`, `build/.${s.env.NODE_ENV}.env`);
 s.cp('jwtRS256.key', 'build/');
 s.cp('jwtRS256.key.pub', 'build/');
 s.cp('-R', 'public', 'build/public');
