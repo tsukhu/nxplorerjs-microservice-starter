6082a8f463fcb4d751bfad4fe8cd14480bad7631
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-04-10 17:25:36 +0530

    Typescript upgrade

--- server/api/services/starwars-service.spec.ts
+++ server/api/services/starwars-service.spec.ts
@@ -6,17 +6,21 @@ import ILogger from '../../common/interfaces/ilogger';
 import IStarwars from '../interfaces/istarwars';
 
 describe('Starwars Service Tests', () => {
-    let starWarsService: IStarwars;
-    let testTimeOut = +process.env.TEST_TIMEOUT;
-    beforeEach(() => {
-        starWarsService = container.get<IStarwars>(SERVICE_IDENTIFIER.STARWARS);
-    });
-    
-    it('can get person of id 1 with homeworld as Tatooine ', done => {
-        starWarsService.getPeopleById(1).subscribe((results: People) => {
-          expect(results.name).toEqual('Luke Skywalker');
-          expect(results.homeworld.name).toEqual('Tatooine');
-          done();
-        });
-      },testTimeOut);
-})
+  let starWarsService: IStarwars;
+  const testTimeOut = +process.env.TEST_TIME_OUT;
+  beforeEach(() => {
+    starWarsService = container.get<IStarwars>(SERVICE_IDENTIFIER.STARWARS);
+  });
+
+  it(
+    'can get person of id 1 with homeworld as Tatooine ',
+    done => {
+      starWarsService.getPeopleById(1).subscribe((results: People) => {
+        expect(results.name).toEqual('Luke Skywalker');
+        expect(results.homeworld.name).toEqual('Tatooine');
+        done();
+      });
+    },
+    testTimeOut
+  );
+});
