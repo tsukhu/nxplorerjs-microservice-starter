b996f34790f52686d25dce8fd4d6123cb0619802
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-03-07 17:15:41 +0530

    added cors support

--- server/common/server.ts
+++ server/common/server.ts
@@ -36,6 +36,12 @@ export default class ExpressServer {
       // Add security configuration
       secureApp(app);
 
+      app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+      });
+      
       // Add public folder
       app.use(express.static(`${root}/public`));
 
