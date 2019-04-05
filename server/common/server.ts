fd05e491fbfdaae30ff665c47ae1e9568b192bd5
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2019-04-05 15:08:52 +0530

    added delete support

--- server/common/server.ts
+++ server/common/server.ts
@@ -38,10 +38,14 @@ export default class ExpressServer {
 
       app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Origin, X-Requested-With, Content-Type, Accept'
+        );
         next();
       });
-      
+
       // Add public folder
       app.use(express.static(`${root}/public`));
 
